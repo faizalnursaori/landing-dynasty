3b86402d5339ff7b016182d94e17f7e75b0c6378
Faizal Nursaori <faizalrahmatn@gmail.com>
2025-07-07 10:09:16 +0700

    feat: add Link on gallery

--- src/components/Main/GalleryDynasty.tsx
+++ src/components/Main/GalleryDynasty.tsx
@@ -1,5 +1,6 @@
 import Image from "next/image";
 import Marquee from "react-fast-marquee";
+import Link from "next/link";
 
 export const GalleryDynasty = () => {
   return (
@@ -8,13 +9,18 @@ export const GalleryDynasty = () => {
         <Marquee gradient={false} speed={70} className="flex gap-4">
           {Array.from({ length: 6 }, (_, i) => (
             <div key={i} className="p-2">
-              <Image
-                src={`/dynasty-square-${i + 1}.jpg`}
-                alt="All of Dynasty"
-                width={300}
-                height={300}
-                className="rounded-xl shadow-lg hover:scale-105 transition-transform duration-300"
-              />
+              <Link
+                href="https://drive.google.com/drive/folders/1-24gICgCcW3pXUUEw__uUR64a46lDzKM"
+                target="_blank"
+              >
+                <Image
+                  src={`/dynasty-square-${i + 1}.jpg`}
+                  alt="All of Dynasty"
+                  width={300}
+                  height={300}
+                  className="rounded-xl shadow-lg hover:scale-105 transition-transform duration-300"
+                />
+              </Link>
             </div>
           ))}
         </Marquee>
